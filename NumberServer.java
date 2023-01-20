8dbb86a76c5480188bf7d10d68405cc95e389ef6
scbalatbat <scsb1119@gmail.com>
2023-01-19 17:25:33 -0800

    Update NumberServer.java

--- NumberServer.java
+++ NumberServer.java
@@ -6,22 +6,34 @@ class Handler implements URLHandler {
     // various requests.
     int num = 0;
 
+    private static final int FUNNY_NUMBER = 69;
+
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Merp Number: %d", num);
+            return String.format("Hello there.\nMerp Number: %d", num);
         } else if (url.getPath().equals("/increment")) {
             num += 1;
-            return String.format("Number incremented!") + "\nGet Rekt!";
+            if (num==FUNNY_NUMBER) {
+                return String.format("Number incremented!") + "\nNice.";
+            }
+            else {
+                return String.format("Number incremented!") + "\nGet Rekt!";
+            }
         } else {
             System.out.println("Path: " + url.getPath());
             if (url.getPath().contains("/add")) {
                 String[] parameters = url.getQuery().split("=");
                 if (parameters[0].equals("count")) {
                     num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num) + "\nRoasted!";
+                    if (num==FUNNY_NUMBER) {
+                        return String.format("Number increased by %s! It's now %d", parameters[1], num) + "\nNice.";
+                    }
+                    else {
+                        return String.format("Number increased by %s! It's now %d", parameters[1], num) + "\nRoasted!";
+                    }
                 }
             }
-            return "404 Not Found!";
+            return "404 Not Found!\nGit gud!";
         }
     }
 }
